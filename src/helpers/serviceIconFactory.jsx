fcaa73ba0004f2798dd10ae256b86648a0c996e2
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-01-09 22:59:41 +0200

    integration

--- src/helpers/serviceIconFactory.jsx
+++ src/helpers/serviceIconFactory.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-// import JesusIcon from '../../assets/svg/icon-jesus.inline.svg';
-// import CamelIcon from '../../assets/svg/icon_camel.inline.svg';
-// import TempleIcon from '../../assets/svg/icon-temple.inline.svg';
+import { ReactComponent as JesusIcon } from '../assets/svg/icon-jesus.inline.svg';
+import { ReactComponent as CamelIcon } from '../assets/svg/icon_camel.inline.svg';
+import { ReactComponent as TempleIcon } from '../assets/svg/icon-temple.inline.svg';
 
-export const iconsFactory = {
-  // jesusIcon: <JesusIcon />,
-  // camelIcon: <CamelIcon className="camel" />,
-  // templeIcon: <TempleIcon />,
+const iconsFactory = {
+  jesusIcon: <JesusIcon />,
+  camelIcon: <CamelIcon className="camel" />,
+  templeIcon: <TempleIcon />,
 };
+
+export default iconsFactory;
