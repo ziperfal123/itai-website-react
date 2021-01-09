25a9f964626c9fd579eb0e03e98a8fde3a329ff5
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-01-09 17:20:20 +0200

    integration

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import {BrowserRouter, Link, Switch} from 'react-router-dom'
-const LANGUAGES = {
-  english: 'en',
-  spanish: 'es',
-};
+// import { slide as Menu } from 'react-burger-menu';
+import { BrowserRouter, Link, Switch } from 'react-router-dom';
 
-const Nav = () => {
-  return (
-      <BrowserRouter>
-      <div className={'navigation'}>
-      {/*<Menu right>*/}
-      {/*  <Link to="/" className="link">{'home'}</Link>*/}
-      {/*  <Link to="/about/" className="link">{'about'}</Link>*/}
-      {/*  <Link to="/services/" className="link">{'services'}</Link>*/}
-      {/*  <Link to="/trips/" className="link">{'trips and sites'}</Link>*/}
-      {/*</Menu>*/}
-        <Switch>
+// const LANGUAGES = {
+//   english: 'en',
+//   spanish: 'es',
+// };
+
+const Nav = () => (
+  <BrowserRouter>
+    <div className="navigation">
+      {/* <Menu right> */}
+      {/*  <Link to="/" className="link">{'home'}</Link> */}
+      {/*  <Link to="/about/" className="link">{'about'}</Link> */}
+      {/*  <Link to="/services/" className="link">{'services'}</Link> */}
+      {/*  <Link to="/trips/" className="link">{'trips and sites'}</Link> */}
+      {/* </Menu> */}
+      <Switch>
         <nav className="navigation">
-        <ul>
-          <Link to="/" className="link">{'home'}</Link>
-          <Link to="/about/" className="link">{'about'}</Link>
-          <Link to="/services/" className="link">{'services'}</Link>
-          <Link to="/trips/" className="link">{'trips and sites'}</Link>
-        </ul>
-      </nav>
-    </Switch>
-      </div>
-      </BrowserRouter>
-  );
-};
+          <ul>
+            <Link to="/" className="link">home</Link>
+            <Link to="/about/" className="link">about</Link>
+            <Link to="/services/" className="link">services</Link>
+            <Link to="/trips/" className="link">trips and sites</Link>
+          </ul>
+        </nav>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default Nav;
