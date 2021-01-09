2c520baa9f3c51beb9b7b378d7b1e2df8774605b
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-01-09 23:29:41 +0200

    integration

--- src/components/SocialNetworkHeader/SocialNetworkHeader.jsx
+++ src/components/SocialNetworkHeader/SocialNetworkHeader.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-// import TwitterIcon from '../../assets/svg/twitter.inline.svg';
-// import FacebookIcon from '../../assets/svg/facebook.inline.svg';
-// import ReactComponent from '../../assets/svg/linkedin.inline.svg';
 import SocialNetworkIcons from '../SocialNetworkIcons';
 
-// eslint-disable-next-line no-unused-vars,react/prop-types
-const SocialNetworkHeader = ({ horizontallyCentered }) => (
+const SocialNetworkHeader = () => (
   <div className="social-network-header">
     <SocialNetworkIcons />
   </div>
