95f60d0f06788c7adf5b42a274dfed7a03c2837a
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-01-16 18:33:59 +0200

    style fixes

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FiPhone } from 'react-icons/fi';
 import { FaMapPin } from 'react-icons/fa';
 import { AiOutlineMail } from 'react-icons/ai';
+import { useTranslation } from 'react-i18next';
 import SocialNetworkHeader from '../SocialNetworkHeader';
 import ContactForm from '../contactForm';
 
@@ -9,30 +10,31 @@ const ICON_SIZE = '50';
 const MAIL_ICON_SIZE = '54';
 
 const ContactSection = () => {
+  const { t } = useTranslation();
   const contactMeElements = [
     {
       icon: <FaMapPin size={ICON_SIZE} color="white" />,
-      title: 'Address',
+      title: t('address'),
       details: 'Tel-Aviv, Israel',
     },
     {
       icon: <FiPhone size={ICON_SIZE} color="white" />,
-      title: 'Phone',
+      title: t('phone'),
       details: '+972-525-123456',
     },
     {
       icon: <AiOutlineMail size={MAIL_ICON_SIZE} color="white" />,
-      title: 'Email',
+      title: t('email'),
       details: 'ziperfal123@gmail.com',
     },
   ];
 
-  const renderContactMeDetails = (elem) => (
-    <div className="detail" key={elem.title}>
-      {elem.icon}
+  const renderContactMeDetails = ({ title, icon, details }) => (
+    <div className="detail" key={title}>
+      {icon}
       <div className="text">
-        <h2>{elem.title}</h2>
-        <p>{elem.details}</p>
+        <h2>{title}</h2>
+        <p>{details}</p>
       </div>
     </div>
   );
