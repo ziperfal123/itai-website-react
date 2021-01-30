2585f99112512917b05c990b96830a3e8245ba62
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-01-30 22:57:24 +0200

    move data to external file

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,51 +1,8 @@
 import React from 'react';
 import './servicesPage.css';
-import Jerusalem from '../../assets/jerusalem.jpg';
 import Footer from '../../components/Footer';
 import HeaderSpace from '../../components/HeaderSpace';
-
-const data = [
-  {
-    image: () => (
-      <img
-        alt="one of itai's services "
-        src={Jerusalem}
-      />
-    ),
-    header: 'Old city of jerusalem',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
-  },
-  {
-    image: () => (
-      <img
-        alt="one of itai's services "
-        src={Jerusalem}
-      />
-    ),
-    header: 'Christianity History',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
-  },
-  {
-    image: () => (
-      <img
-        alt="one of itai's services "
-        src={Jerusalem}
-      />
-    ),
-    header: 'Old city of jerusalem',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
-  },
-  {
-    image: () => (
-      <img
-        alt="one of itai's services "
-        src={Jerusalem}
-      />
-    ),
-    header: 'Christianity History',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
-  },
-];
+import itemsData from './itemsData';
 
 const renderItem = ({ image, header, text }) => (
   <div className="item" key={header}>
@@ -61,7 +18,7 @@ const ServicesPage = () => (
   <>
     <HeaderSpace />
     <div className="services page">
-      {data.map(renderItem)}
+      {itemsData.map(renderItem)}
     </div>
     <Footer />
   </>
---
+++ src/pages/ServicesPage/itemsData.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Jerusalem from '../../assets/jerusalem.jpg';
+
+const itemsData = [
+  {
+    image: () => (
+      <img
+        alt="one of itai's services "
+        src={Jerusalem}
+      />
+    ),
+    header: 'Old city of jerusalem',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+  },
+  {
+    image: () => (
+      <img
+        alt="one of itai's services "
+        src={Jerusalem}
+      />
+    ),
+    header: 'Christianity History',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+  },
+  {
+    image: () => (
+      <img
+        alt="one of itai's services "
+        src={Jerusalem}
+      />
+    ),
+    header: 'Old city of jerusalem',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+  },
+  {
+    image: () => (
+      <img
+        alt="one of itai's services "
+        src={Jerusalem}
+      />
+    ),
+    header: 'Christianity History',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+  },
+];
+
+export default itemsData;
