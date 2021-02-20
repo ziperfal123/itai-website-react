16158942f622f6f3ff3d96880087cfaae6dc3b6f
Yaniv Ziperfal <ziperfal123@gmail.com>
2021-02-20 23:34:31 +0200

    common trips and sites page

--- src/components/Carousel/SideNav.jsx
+++ src/components/Carousel/SideNav.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const itemsText = ['nazeret', 'death sea', 'jerusalem', 'haifa', 'eilat'];
 
 const SideNav = () => {
   console.log('SideNav');
+  const [selectedItem, setSelectedItem] = useState((itemsText.length - 1) / 2);
+  console.log('setSelectedItem: ', setSelectedItem);
+  const renderItem = (textToDisplay, index) => (
+    <li
+      className={index === selectedItem && 'selected'}
+      key={textToDisplay}
+
+    >
+      {textToDisplay}
+    </li>
+  );
+
   return (
     <ul className="side-nav">
-      <li>111</li>
-      <li>222</li>
-      <li>333</li>
-      <li>444</li>
-      <li>555</li>
+      {itemsText.map(renderItem)}
     </ul>
   );
 };
